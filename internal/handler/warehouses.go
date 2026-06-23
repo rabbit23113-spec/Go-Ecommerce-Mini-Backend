665f1600b14838f8b8347d4d573eabfcdd65d401
Raffinnaisse <rabbit23113@gmail.com>
2026-06-23 15:49:55 +0300

    warehouses: all methods except update

--- internal/handler/warehouses.go
+++ internal/handler/warehouses.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"main/internal/service"
+	"main/package/dto"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -12,15 +13,36 @@ type WarehousesHandler struct {
 }
 
 func (wh *WarehousesHandler) Find(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, "mock find")
+	resp, err := wh.Service.WarehouseService.Find()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, resp)
 }
 
 func (wh *WarehousesHandler) FindByProductId(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, "mock find by product id")
+	productId, ok := ctx.Params.Get("id")
+	if ok != true {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "You should provide a product id"})
+	}
+	resp, err := wh.Service.WarehouseService.FindByProductId(productId)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, resp)
 }
 
 func (wh *WarehousesHandler) Create(ctx *gin.Context) {
-	ctx.JSON(http.StatusCreated, "mock create")
+	var body dto.CreateWarehouseDto
+	ctx.ShouldBindJSON(&body)
+	resp, err := wh.Service.WarehouseService.Create(body)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusCreated, resp)
 }
 
 func (wh *WarehousesHandler) Update(ctx *gin.Context) {
@@ -28,5 +50,14 @@ func (wh *WarehousesHandler) Update(ctx *gin.Context) {
 }
 
 func (wh *WarehousesHandler) Delete(ctx *gin.Context) {
-	ctx.JSON(http.StatusNoContent, "mock delete")
+	id, ok := ctx.Params.Get("id")
+	if ok != true {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "You should provide a product id"})
+	}
+	err := wh.Service.WarehouseService.Delete(id)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusNoContent, "")
 }
--- internal/service/warehouses.go
+++ internal/service/warehouses.go
@@ -1,22 +1,46 @@
 package service
 
-import "github.com/jmoiron/sqlx"
+import (
+	"main/package/dto"
+
+	"github.com/jmoiron/sqlx"
+)
 
 type WarehouseService struct {
 	Postgres *sqlx.DB
 }
 
-func (ws *WarehouseService) Find() {
+func (ws *WarehouseService) Find() ([]dto.WarehouseDto, error) {
+	var warehouses []dto.WarehouseDto
+	if err := ws.Postgres.Get(&warehouses, "SELECT * FROM warehouses"); err != nil {
+		return nil, err
+	}
+	return warehouses, nil
 }
 
-func (ws *WarehouseService) FindByProductId() {
+func (ws *WarehouseService) FindByProductId(productId string) ([]dto.WarehouseDto, error) {
+	var warehouses []dto.WarehouseDto
+	if err := ws.Postgres.Get(&warehouses, "SELECT * FROM warehouses WHERE $1 IN product_ids", productId); err != nil {
+		return nil, err
+	}
+	return warehouses, nil
 }
 
-func (ws *WarehouseService) Create() {
+func (ws *WarehouseService) Create(req dto.CreateWarehouseDto) (dto.WarehouseDto, error) {
+	var warehouse dto.WarehouseDto
+	if err := ws.Postgres.Get(&warehouse, "INSERT INTO warehouses (location, status) VALUES ($1, $2) RETURNING *", req.Location, req.Status); err != nil {
+		return dto.WarehouseDto{}, err
+	}
+	return warehouse, nil
 }
 
 func (ws *WarehouseService) Update() {
 }
 
-func (ws *WarehouseService) Delete() {
+func (ws *WarehouseService) Delete(id string) error {
+	_, err := ws.Postgres.Exec("DELETE FROM warehouses WHERE id = $1", id)
+	if err != nil {
+		return err
+	}
+	return nil
 }
