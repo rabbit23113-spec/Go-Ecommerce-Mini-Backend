31ad3a49c4806b2a030928da266abfabd0306d89
Raffinnaisse <rabbit23113@gmail.com>
2026-06-23 15:18:27 +0300

    products: all methods except update

--- internal/handler/products.go
+++ internal/handler/products.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"main/internal/service"
+	"main/package/dto"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -12,11 +13,23 @@ type ProductsHandler struct {
 }
 
 func (ph *ProductsHandler) Find(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, "mock find")
+	resp, err := ph.Service.ProductService.Find()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, resp)
 }
 
 func (ph *ProductsHandler) Create(ctx *gin.Context) {
-	ctx.JSON(http.StatusCreated, "mock create")
+	var body dto.CreateProductDto
+	ctx.ShouldBindJSON(&body)
+	resp, err := ph.Service.ProductService.Create(body)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusCreated, resp)
 }
 
 func (ph *ProductsHandler) Update(ctx *gin.Context) {
@@ -24,5 +37,13 @@ func (ph *ProductsHandler) Update(ctx *gin.Context) {
 }
 
 func (ph *ProductsHandler) Delete(ctx *gin.Context) {
-	ctx.JSON(http.StatusNoContent, "mock delete")
+	id, ok := ctx.Params.Get("id")
+	if ok != true {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "you should define an id parameter"})
+		return
+	}
+	if err := ph.Service.ProductService.Delete(id); err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
+	ctx.JSON(http.StatusNoContent, "")
 }
--- internal/service/products.go
+++ internal/service/products.go
@@ -1,23 +1,39 @@
 package service
 
-import "github.com/jmoiron/sqlx"
+import (
+	"main/package/dto"
+
+	"github.com/jmoiron/sqlx"
+)
 
 type ProductService struct {
 	Postgres *sqlx.DB
 }
 
-func (ps *ProductService) Find() {
+func (ps *ProductService) Find() ([]dto.ProductDto, error) {
+	var products []dto.ProductDto
 
+	if err := ps.Postgres.Get(&products, "SELECT * FROM products"); err != nil {
+		return nil, err
+	}
+	return products, nil
 }
 
-func (ps *ProductService) Create() {
-
+func (ps *ProductService) Create(req dto.CreateProductDto) (dto.ProductDto, error) {
+	var product dto.ProductDto
+	if err := ps.Postgres.Get("INSERT INTO products (name, price) VALUES ($1, $2)", req.Name, req.Price); err != nil {
+		return dto.ProductDto{}, err
+	}
+	return product, nil
 }
 
 func (ps *ProductService) Update() {
 
 }
 
-func (ps *ProductService) Delete() {
-
+func (ps *ProductService) Delete(id string) error {
+	if _, err := ps.Postgres.Exec("DELETE FROM products WHERE id = $1", id); err != nil {
+		return err
+	}
+	return nil
 }
